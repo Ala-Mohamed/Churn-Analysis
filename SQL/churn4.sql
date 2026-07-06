ccb3c126b74eb938f7210b5d71515387e18fd7fe
Ala-Mohamed <alaaelmansy325@gmail.com>
2026-07-06 22:57:24 +0300

    Add files via upload

---
+++ SQL/churn4.sql
@@ -0,0 +1,5 @@
+Create View vw_ChurnData AS
+    select * from prod_Churn where Customer_Status In ('Churned', 'Stayed')
+GO
+Create View vw_JoinData as
+    select * from prod_Churn where Customer_Status = 'Joined'
